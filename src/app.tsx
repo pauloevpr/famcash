93a62d61de5b3e1867d1ef10291c912bce977cbc
Paul Roque <paulo_evpr@hotmail.com>
2024-10-01 16:27:59 +1000

    clean up unused stuff from template

--- src/app.tsx
+++ src/app.tsx
@@ -9,8 +9,6 @@ export default function App() {
     <Router
       root={props => (
         <>
-          <a href="/">Index</a>
-          <a href="/about">About</a>
           <Suspense>{props.children}</Suspense>
         </>
       )}
